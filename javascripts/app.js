0ba5732667a4103b11ab7a086a91de7af19f598e
Alain Gilbert <alain.gilbert.15@gmail.com>
2015-04-19 22:30:58 -0700

    Update 2015-04-20T05:30:57.099Z

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,25 +1,25 @@
 'use strict';
 
 angular.module('exampleApp', [
-    'ngRoute',
-    'angular-dropdown-multiselect',
-    'hljs',
-	'ui.bootstrap'
+  'ngRoute',
+  'angular-dropdown-multiselect',
+  'hljs',
+  'ui.bootstrap'
 ]).
 config(['$routeProvider', '$locationProvider', '$compileProvider',
-    function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode(false);
+  function($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
 
 
-        var appPathRoute = '/';
-        var pagesPath = staticPath + 'javascripts/pages/';
+    var appPathRoute = '/';
+    var pagesPath = staticPath + 'javascripts/pages/';
 
 
-        $routeProvider.when('/', {
-            templateUrl: pagesPath + 'home/home.html'
-        });
+    $routeProvider.when('/', {
+      templateUrl: pagesPath + 'home/home.html'
+    });
 
-        $routeProvider.otherwise('/');
+    $routeProvider.otherwise('/');
 
-    }
+  }
 ]);
